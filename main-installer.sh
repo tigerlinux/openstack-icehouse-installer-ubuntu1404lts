043f95663cbdc5cb8e72bc296c5a75295ee9808b
Reynaldo R. Martinez P <tigerlinux@gmail.com>
2015-01-11 20:51:44 -0430

    Commit para v1.1.4.ub1404lts "Snow Lynx"
    - Se modifica la verificación de instalación de ceilometer
      para adaptarlo a nodos de compute.

--- main-installer.sh
+++ main-installer.sh
@@ -12,7 +12,7 @@
 #
 # Script principal
 # Versión 1.1.3.ub1404lts "Snow Lynx"
-# 22 de Octubre del 2014
+# 11 de Enero del 2015
 #
 
 PATH=$PATH:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
@@ -43,7 +43,7 @@ case $1 in
 	echo "INSTALADOR DE OPENSTACK ICEHOUSE PARA UBUNTU SERVER 14.04 LTS"
 	echo "Realizado por Reynaldo R. Martinez P."
 	echo "E-Mail: TigerLinux@Gmail.com"
-	echo "Versión 1.1.3.ub1404lts \"Snow Lynx\" - Noviembre 29, 2014"
+	echo "Versión 1.1.4.ub1404lts \"Snow Lynx\" - Enero 11, 2015"
 	echo ""
 	echo "Se verificaran los prerequisitos"
 	echo "Si alguno de los prerequisitos falla, se informará y se detendrá el proceso"
